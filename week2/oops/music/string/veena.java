6b1a118f884645b85a77879ee58259ef7ec091c9
pulkitvars-hney <pulkitvarshney2402@gmail.com>
2026-06-14 09:56:20 +0530

    oops done

---
+++ week2/oops/music/string/veena.java
@@ -0,0 +1,11 @@
+package music.string;
+
+import music.Playable;
+
+public class veena implements playable {
+
+    @Override
+    public void play() {
+        System.out.println("Playing Veena");
+    }
+}
